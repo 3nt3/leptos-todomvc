1bcd4021ab601b2d546a67a0302342e551aef7d3
3nt3 <gott@3nt3.de>
2022-12-30 11:15:58 +0100

    i made this woohoo
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,51 @@
 use leptos::*;
 
 #[component]
-pub fn SimpleCounter(cx: Scope) -> impl IntoView {
-    let (counter, set_counter) = create_signal(cx, -1);
+pub fn Todos(cx: Scope) -> impl IntoView {
+    let (todos, set_todos) = create_signal::<Vec<String>>(cx, vec![]);
+    let (input_value, set_input_value) = create_signal(cx, String::new());
 
-    let reset_counter = move |_| set_counter(3);
+    let input = move |event| set_input_value.set(event_target_value(&event));
+    let add_todo = move |_| {
+        set_todos.update(|todos| todos.push(input_value()));
+        console_log(format!("{:?}", todos().len()).as_str());
+    };
+    let clear_todos = move |_| {
+        set_todos.update(|todos| *todos = vec![]);
+        set_input_value.set(String::new());
+    };
+
+    let remove_todo = move |x: String| {
+        set_todos.update(|todos| {
+            *todos = todos
+                .iter()
+                .filter(|todo| **todo != x)
+                .map(|todo| todo.to_owned())
+                .collect()
+        });
+    };
 
     view! { cx,
         <div>
-            <span>"counter: " {counter}</span>
-            <button on:click=reset_counter >"+"</button>
+            <h1>"TODOS"</h1>
+            <span>"n todos: "{move || todos().len()}</span>
+            <button on:click=clear_todos>"Clear Todos"</button>
+            <input on:input=input />
+            <button on:click=add_todo>"Add Todo"</button>
+            <ul>
+                <For
+                    each=todos
+                    key=|todo| todo.clone()
+                    view=move |todo: String| {
+                        view! {cx,
+                            <li>
+                                <span>{todo.clone()}</span>
+                                <button on:click=move |_| remove_todo(todo.clone())>"Remove Todo"</button>
+                            </li>
+                        }
+                    }
+                />
+            </ul>
         </div>
     }
 }
